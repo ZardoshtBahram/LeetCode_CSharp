1a2c9756888a6516568e5ca4822275a0356dcb0d
Bahram Zardosht <zardoshtbahram81@gmail.com>
2025-03-21 15:42:28 -0400

    2215

---
+++ LeetCode/2115_AllPossiableRecipes.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class _2115_AllPossiableRecipes
+    {
+        public IList<string> FindAllRecipes(string[] recipes, IList<IList<string>> ingredients, string[] supplies)
+        {
+            IList<string> result = new List<string>();
+            Dictionary<string , List<string>> recipesToIng = new Dictionary<string , List<string>>();
+            Dictionary<string , int> visited = new Dictionary<string , int>();
+            HashSet<string> avalableSupplies = new HashSet<string>(supplies);
+
+            for (int i = 0; i < recipes.Length; i++)
+                recipesToIng.Add(recipes[i], new List<string>(ingredients[i]));
+
+
+            bool canMake (string recipe) 
+            {
+                if (visited.ContainsKey(recipe)) 
+                    return visited[recipe] == 1;
+
+                if (avalableSupplies.Contains(recipe)) 
+                    return true; 
+
+                if (!recipesToIng.ContainsKey(recipe))
+                    return false;
+
+                visited[recipe] = 0;
+
+                foreach (string ing in recipesToIng[recipe]) 
+                {
+                    if (!canMake(ing)) 
+                    {
+                        visited[recipe] = -1;
+                        return false;
+                    }
+                }
+
+                visited[recipe] = 1;
+                result.Add(recipe);
+                return true;
+
+            }
+
+            foreach (string rec in recipes) 
+                canMake (rec);
+
+            return result;
+            
+        }
+        
+    }
+
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace LeetCode
+﻿using System.Diagnostics;
+
+namespace LeetCode
 {
     internal class Program
     {
+        // Create a class for each problem you work on 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Stopwatch timer = new Stopwatch();
+            _2115_AllPossiableRecipes test = new _2115_AllPossiableRecipes();
+            timer.Start();
+
+            List<string> rec = new List<string>(test.FindAllRecipes(["bread"], [["yeast", "flour"]], ["yeast", "flour", "corn"]));
+
+
+
+            timer.Stop();
+            Console.WriteLine(timer.ElapsedMilliseconds);
         }
     }
 }
