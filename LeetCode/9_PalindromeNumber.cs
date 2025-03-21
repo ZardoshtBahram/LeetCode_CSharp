4ee5b6379a4d7df7a710ed3cc8f32d58cef7fc8c
Bahram Zardosht <zardoshtbahram81@gmail.com>
2025-03-21 18:07:53 -0400

    some change to the is palindrom method

---
+++ LeetCode/9_PalindromeNumber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class _9_PalindromeNumber
+    {
+        public bool IsPalindrome(int x)
+        {
+            if (x < 0)
+                return false;
+
+            bool IsPalin = true;
+            string str = x.ToString();
+
+            for (int i = 0; i < str.Length/2; i++) 
+            {
+                if (str[i] != str[str.Length - i - 1])
+                {
+                    IsPalin = false;
+                    break;
+                }
+                
+            }
+            return IsPalin;
+        }
+    }
+}
